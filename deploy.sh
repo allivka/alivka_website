f6ce100484d48e23b80ed302a2d2ff32af77cdce
allivka <allivka1920@gmail.com>
2026-01-18 19:04:52 +0300

    added walatime 2025 stats

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 sudo git pull
-sudo cp index.html $1/
-sudo cp -r html $1/
-sudo cp -r css $1/
-sudo cp -r images $1/
+sudo cp -r ./* html $1/
 sudo nginx -t
 sudo systemctl reload nginx.service
